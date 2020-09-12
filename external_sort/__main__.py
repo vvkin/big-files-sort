95c1afc31e7a71d2af8824008189d4e97bb7f5ed
Vadym Kinchur <vkincur@gmail.com>
2020-09-12 04:53:01 +0300

    Add logic to ExternalSort, add check for file existence in __main__.py

--- external_sort/__main__.py
+++ external_sort/__main__.py
@@ -9,7 +9,11 @@ from sort_tools import (
 def main():
     if len(sys.argv) - ('-i' in sys.argv) != 2:  # ./script [-i] file_name
         sys.exit('Incorrect number of parameters')
+    
     file_name = [arg for arg in sys.argv[1:] if arg != '-i'][0]
+    if not os.path.isfile(file_name):
+        sys.exit(f"Path '{file_name}' does not exist or is inaccessible")
+    
     f_name, extension = os.path.splitext(file_name)
     sorted_name = f_name + '_sorted' + extension
     if '-i' in sys.argv[1:]: # use internal sort and delete unsorted file
@@ -18,6 +22,6 @@ def main():
     else: # rename file
         os.rename(file_name, sorted_name)
     external_sort(sorted_name)
-
+        
 if __name__ == '__main__':
     main()
--- external_sort/sort_tools/external_sort.py
+++ external_sort/sort_tools/external_sort.py
@@ -15,8 +15,23 @@ class ExternalSort(object):
         self._temp_files = ['b_file', 'c_file']
     
     def __call__(self):
-        pass
+        """
+        Method to make class callable
+        executes split and merge while
+        file is not sorted
+        """
+        #while not self.__is_sorted():
+        #    self.__split()
+        #    self.__merge()
     
+    def __split(self) -> None:
+        """Split target file in two files by series"""
+        pass
+
+    def __merge(self) -> None:
+        """Merge temp files and place it in target file"""
+        pass
+
     @staticmethod
     def __get_file_size(file_name: str) -> int:
         """Return file size in bytes"""
@@ -27,8 +42,21 @@ class ExternalSort(object):
         """Close list of binary files"""
         for file in files:
             file.close()
+    
+    def __delete_temp_files(self) -> None:
+        """Delete temp files used in sorting"""
+        for file_name in self._temp_files:
+            os.remove(file_name)
+    
+    def __is_sorted(self) -> bool:
+        """Check if sorting is over"""
+        return (
+            self.__get_file_size(self._file_name) == self._file_size and
+            self.__get_file_size(self._temp_files[0]) == 0 and
+            self.__get_file_size(self._temp_files[1]) == 0
+        )
 
 def external_sort(file_name: str) -> None:
-    """Wrapper of functor class ExternalSort"""
+    """Wrapper for functor class ExternalSort"""
     obj = ExternalSort(file_name)
     return obj()
